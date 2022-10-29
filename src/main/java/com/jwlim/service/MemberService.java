f5cb5b5207e252c03dd34d104f95e91759387af2
jwlim <jwlim@ykcns.com>
2022-10-29 15:19:46 +0900

    섹션 3. 회원 관리 예제 - 백엔드 개발

---
+++ src/main/java/com/jwlim/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.jwlim.service;
+
+import com.jwlim.domain.Member;
+import com.jwlim.repository.MemberRepository;
+import com.jwlim.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복회원 검증
+     */
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.fildAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
