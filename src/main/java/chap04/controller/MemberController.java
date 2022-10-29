9091ce6b3b40220555f0d84d829f8983e3501455
mjkim <mjkim@pentasecurity.com>
2022-10-29 15:45:05 +0900

    세션4. 스프링 빈과 의존관계 - 컴포넌트 스캔과 자동 의존관계 설정

---
+++ src/main/java/chap04/controller/MemberController.java
@@ -0,0 +1,16 @@
+package chap04.controller;
+
+import chap04.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
---
+++ src/main/java/chap04/service/MemberService.java
@@ -0,0 +1,62 @@
+package chap04.service;
+
+import chap04.domain.Member;
+import chap04.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복 이름 검증
+     * @param member
+     */
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent( m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+
+    /**
+     * 회원 찾기
+     *  - 아이디로 하나의 회원 찾기
+     * @param memberId
+     * @return
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
