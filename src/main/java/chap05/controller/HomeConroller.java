9c25ce958a6b5485da7dba0d7dbf3424d1ce0dc4
mjkim <devanix@gmail.com>
2022-10-31 16:40:07 +0900

    세션5. 회원 관리 예제(웹 MVC 개발) - 회원 웹 기능 - 조회

--- src/main/java/chap05/controller/HomeConroller.java
+++ src/main/java/chap05/controller/HomeConroller.java
@@ -3,8 +3,11 @@ package chap05.controller;
 import chap05.domain.Member;
 import chap05.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeConroller {
@@ -20,11 +23,20 @@ public class HomeConroller {
         return "home";
     }
 
+    /**
+     * 신규 회원 가입 폼 HTML 호출
+     * @return
+     */
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
     }
 
+    /**
+     * 등록
+     * @param form
+     * @return
+     */
     @PostMapping("members/new")
     public String create(MemberForm form) {
         Member member = new Member();
@@ -33,5 +45,13 @@ public class HomeConroller {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
