5e31edb66c7529ebfbfc4613fd75559115227873
jwlim <jwlim@ykcns.com>
2022-11-04 19:43:25 +0900

    섹션 6. 스프링 DB 접근 기술 - 순수 JDBC

--- src/main/java/com/jwlim/repository/MemoryMemberRepository.java
+++ src/main/java/com/jwlim/repository/MemoryMemberRepository.java
@@ -30,7 +30,7 @@ public class MemoryMemberRepository implements MemberRepository {
     }
 
     @Override
-    public List<Member> fildAll() {
+    public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
 
